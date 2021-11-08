10f0ee8031d1048be045054c9996c190bace2262
ChenShahaf <48526807+ChenShahaf@users.noreply.github.com>
2021-11-08 12:36:55 +0200

    Add files via upload

---
+++ tetris.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.*;
+import java.util.*;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+public class tetris extends Application{
+
+//variables
+	
+	
+	
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
